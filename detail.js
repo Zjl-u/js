929357e52583a333450d919717476bff5a3309af
Zjl-u <2373007562@qq.com>
2025-05-20 21:48:01 +0800

    Add files via upload

---
+++ detail.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function () {
+    var preserve_img = document.querySelector('.preserve_img');
+    var mask = document.querySelector('.mask');
+    var big = document.querySelector('.big');
+})
