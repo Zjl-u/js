c46334d843dc63351ecc9f5b434c0ab45e6a6a58
Zjl-u <2373007562@qq.com>
2025-11-16 21:42:46 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,98 @@
+class HD{
+    static PENDING = 'pending';
+    static FULFILLED = 'fulfilled';
+    static REJECTED = 'rejected';
+    constructor(executor) {
+        this.status = HD.PENDING;
+        this.value = null;
+        this.callbacks = [];
+        try {
+            executor(this.resolve.bind(this), this.reject.bind(this));
+        } catch (error) {
+            this.reject(error);
+        }
+    }
+    resolve(value) {
+        if (this.status == HD.PENDING) {
+            this.status = HD.FULFILLED;
+            this.value = value;
+            setTimeout(() => {
+                this.callbacks.map(callback => {
+                    callback.onFulfilled(value);
+                })
+            })
+        }
+    }
+    reject(reason) {
+        if (this.status == HD.PENDING) {
+            this.status = HD.REJECTED;
+            this.value = reason;
+            setTimeout(() => {
+                this.callbacks.map(callback => {
+                    callback.onRejected(reason);
+                })
+            })
+        }
+    }
+    then(onFulfilled, onRejected) {
+        if (typeof onFulfilled != 'function') {
+            onFulfilled = () => this.value;
+        }
+        if (typeof onRejected != 'function') {
+            onRejected = () => this.value;
+        }
+        return new HD((resolve, reject) => {
+            if (this.status == HD.PENDING) {
+                this.callbacks.push({
+                    onFulfilled: (value) => {
+                        try {
+                            let result = onFulfilled(value);
+                            resolve(result);
+                        } catch (error) {
+                            reject(error);
+                        }
+                    },
+                    onRejected: (value) => {
+                        try {
+                            let result = onRejected(value);
+                            resolve(result);
+                        } catch (error) {
+                            reject(error);
+                        }
+                    }
+                })
+            }
+            if (this.status == HD.FULFILLED) {
+                setTimeout(() => {
+                    try {
+                        let result = onFulfilled(this.value);
+                        if (result instanceof HD) {
+                            result.then(value => {
+                                resolve(value);
+                            }, reason => {
+                                reject(reason);
+                            })
+                        } else {
+                            resolve(result);
+                        }
+                    } catch (error) {
+                        reject(error);
+                    }
+                }, 1000)
+            }
+            if (this.status == HD.REJECTED) {
+                setTimeout(() => {
+                    try {
+                        let result = onRejected(this.value);
+                        resolve(result);
+                    } catch (error) {
+                        onRejected(this.value);
+                    }
+                })
+            }
+        })
+    }
+}
+// new Promise((resolve, reject) => {
+    
+// })
